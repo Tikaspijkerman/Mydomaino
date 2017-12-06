970bea9098ad95bf7bb95e3cad4d3605683a7120
Tika Spijkerman <31164427+Tikaspijkerman@users.noreply.github.com>
2017-12-06 16:13:39 +0000

    Update myDomaino.js

--- myDomaino.js
+++ myDomaino.js
@@ -3,3 +3,7 @@ $(document).ready(function () {
 				    $(this).toggleClass("active");
 			  });
 		});
+$(document).ready(function () {
+	$(".send-button").on("click", function showDiv() {
+		document.getElementById('thankyouDiv').style.display = "block";
+	}
